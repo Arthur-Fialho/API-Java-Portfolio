08c6ab04066a5b45fbb98abaa5bc2774c055ece7
Arthur-Fialho <arthursouza.fp@gmail.com>
2025-09-04 16:39:55 -0300

    feat: add render.com config

--- src/main/java/com/arthurfialho/api/config/SecurityConfig.java
+++ src/main/java/com/arthurfialho/api/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/actuator/health").permitAll()
                         .requestMatchers("/auth/login").permitAll()
                         .requestMatchers(HttpMethod.GET, "/projects/**", "/experiences/**", "/educations/**").permitAll()
                         .anyRequest().authenticated()
